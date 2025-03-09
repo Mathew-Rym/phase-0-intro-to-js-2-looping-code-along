a003f4deaa9f14173fda89b18f9659ce21e45c81
mathew rym <rymmathew@gmail.com>
2025-03-09 13:19:43 +0300

    Added Countdown

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+// Function to generate thank you messages
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function to count down from a given number to 0
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Exporting for testing purposes (if needed in Node.js)
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = {
+      writeCards,
+      countDown,
+    };
+  }
+  
